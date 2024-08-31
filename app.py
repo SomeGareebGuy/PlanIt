0bab4b9839d4b912550b14a8b39b7af6ae169288
SomeGareebGuy <dashanubhav50@gmail.com>
2024-08-31 12:25:30 +0530

    Setting up Database

--- app.py
+++ app.py
@@ -1,11 +1,23 @@
 from flask import Flask, request, redirect, session, url_for, render_template
 from flask_session import Session
 from werkzeug.security import generate_password_hash, check_password_hash
+from flask_sqlalchemy import SQLAlchemy
 
+app = Flask(__name__)
 
-app = Flask(__name__) 
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+db = SQLAlchemy(app)
 
 app.config["SESSION_TYPE"] = "filesystem"
+
+class Profile(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(20), unique=True, nullable=False)
+    hash_id = db.Column(db.String(20), unique=True, nullable=False)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+
+    def __repr__(self):
+        return f"Name : {self.first_name}, Age: {self.age}"
 
 Session(app)
 
